ea0eada38a360e5cd87cf3ba44f18deedc3ccea0
maugares <mauro.garciaesteban@gmail.com>
2019-06-25 14:13:53 +0200

    Add Product to admin in the products app

--- products/models.py
+++ products/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Product(models.Model):
   title = models.TextField()
   description = models.TextField()
-  price = models.TextField() 
+  price = models.TextField()
+  summary = models.TextField()
